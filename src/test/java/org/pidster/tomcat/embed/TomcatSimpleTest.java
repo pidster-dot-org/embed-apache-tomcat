0b0c710f1bf9744d332a3713ffde693ae8f92eeb
Pid <pid@pidster.com>
2013-05-21 01:53:58 +0100

    remove unused from test

--- src/test/java/org/pidster/tomcat/embed/TomcatSimpleTest.java
+++ src/test/java/org/pidster/tomcat/embed/TomcatSimpleTest.java
@@ -20,10 +20,6 @@ public class TomcatSimpleTest {
     @Before
     public void setupTomcat() throws Exception {
 
-        Properties properties = new Properties();
-        properties.put("catalina.base", "src/test/resources");
-        properties.put("catalina.home", "src/test/resources");
-
         File catalinaBase = new File("src/test/resources");
 
         Tomcat tomcat = new TomcatFactory().create()
@@ -80,25 +76,6 @@ public class TomcatSimpleTest {
     public void test0Foo() throws Exception {
 
         URL url = new URL("http://127.0.0.1:8090/test0/foo");
-
-        HttpURLConnection connection = connect(url);
-        connection.connect();
-
-        try (InputStream is = connection.getInputStream()) {
-
-            int responseCode = connection.getResponseCode();
-            Assert.assertEquals(200, responseCode);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-            Assert.fail(e.getMessage());
-        }
-    }
-
-    @Test
-    public void test1IndexFile() throws Exception {
-
-        URL url = new URL("http://127.0.0.1:8090/test1/");
 
         HttpURLConnection connection = connect(url);
         connection.connect();
